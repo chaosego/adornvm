e2014335f526ae48e1fad30b153c8bd3fb7ee81f
Robluchy <91990736+Robluchy@users.noreply.github.com>
2023-07-18 19:37:31 +0200

    Update SoftSkills.tsx

--- src/components/SoftSkills.tsx
+++ src/components/SoftSkills.tsx
@@ -1,20 +1,36 @@
+import { useRef, useState } from "react";
+import { motion, useInView } from "framer-motion";
+
 interface Props {
   title: string;
   items: string[];
 }
+
 export default function SoftSkills({ title, items }: Props) {
+  const h1 = useRef<HTMLHeadingElement>(null);
+  const isInView = useInView(h1, { once: true });
   return (
     <section className="pt-20 ">
       <div className="leading-[90px] tracking-[-3px] text-5xl">
         <div>
           <h4 className="my-8 lg:text-4xl text-[#707070] ">{title}</h4>
           {items.map((item, index) => (
-            <h1
+            <motion.h1
+              ref={h1}
               key={index}
               className="text-3xl text-left tracking-[-2px] leading-[70px] "
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={isInView ? { opacity: 1, scale: 1 } : { opacity: 0 }}
+              transition={{
+                type: "spring",
+                stiffness: 250,
+                damping: 150,
+                mass: 1,
+                delay: 0.01 + index * 0.1,
+              }}
             >
               {item}
-            </h1>
+            </motion.h1>
           ))}
         </div>
       </div>
