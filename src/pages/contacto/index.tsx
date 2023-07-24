9c8066e31d5925dd4fde369bfc262235fbe57908
Robluchy <91990736+Robluchy@users.noreply.github.com>
2023-07-24 19:56:08 +0200

    📸

--- src/pages/contacto/index.tsx
+++ src/pages/contacto/index.tsx
@@ -2,12 +2,12 @@
 import Hero from "@/components/Hero";
 import { Layout } from "@/components/Layout";
 import Skills from "@/components/SoftSkills";
-import Trabajos from "@/components/Trabajos";
 import { Line } from "@/components/ui/Line";
 import { programs, skills } from "@/data/dto";
 import { supabase } from "@/supabase/config";
 import Works from "@/supabase/works";
 import { motion, useInView } from "framer-motion";
+import Link from "next/link";
 import { useEffect, useRef } from "react";
 
 interface Props {
@@ -84,7 +84,30 @@ export default function Home({ proyectos }: Props) {
         <div className="mx-8 lg:mx-16 xl:mx-24  flex justify-center items-center ">
           <span className="h-[2px] block bg-white w-full" />
         </div>
-        <Trabajos works={proyectos} />
+        <section className="mx-8 lg:mx-16 xl:mx-24 grid grid-cols-1 md:grid-cols-6 lg:grid-cols-12 lg:my-40 ">
+          <div className="flex col-span-full md:col-start-1 md:col-end-4 items-start pt-7">
+            <div className="lg:rotate-180 mb-11 uppercase lg:[writing-mode:_vertical-lr]">
+              <h3>Trabajos</h3>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-start-4 lg:col-end-13 flex items-center leading-[80px] md:leading-[150px] text-[7vw] md:text-[5vw] pt-20 tracking-[-1px]">
+            <div>
+              {proyectos.map((proyectos, index) => (
+                <div
+                  key={index}
+                  className=" lg:hover:cursor-pointer lg:hover:opacity-100 w-auto lg:opacity-50"
+                >
+                  <Link
+                    href={`work/${proyectos.id}`}
+                    className=" font-normal text-left"
+                  >
+                    {proyectos.id}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
         <div className="mx-8 lg:mx-16 xl:mx-24 pt-12 flex justify-center items-center ">
           <Line bgColor=" bg-[#EBE4E4] " />
         </div>
--- src/supabase/config.ts
+++ src/supabase/config.ts
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kiubjdmfcguvgiurzqpq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpdWJqZG1mY2d1dmdpdXJ6cXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgzNzYyOTYsImV4cCI6MjAwMzk1MjI5Nn0.tEPuYlzQa7W1Ofp46gv96NuMllm60DC7xj7p9iFHiN0";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables");
 }
-const supabase = createClient(supabaseUrl, supabaseKey, {auth: {persistSession: false}});
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: { persistSession: false },
+});
 
 export { supabase };
-
