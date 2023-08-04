48250069757a9ba8213bfeb697010d13c5d587f2
Robluchy <91990736+Robluchy@users.noreply.github.com>
2023-08-04 23:10:53 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -129,7 +129,7 @@ export default function Home({ proyectos }: Props) {
                   />
                 </motion.button>
               </div>
-            )}VW
+            )}
           </div>
         </section>
 
