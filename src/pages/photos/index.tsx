82abd4d784a539bde91dd0b87127042ce4e0b981
Robluchy <91990736+Robluchy@users.noreply.github.com>
2023-08-07 01:22:19 +0200

    Update index.tsx

--- src/pages/photos/index.tsx
+++ src/pages/photos/index.tsx
@@ -1,29 +1,11 @@
 import { Layout } from "@/components/Layout";
 import { supabase } from "@/supabase/config";
 import Card from "./components/Card";
-import { useEffect, useState } from "react";
 type Image = {
   name: string;
 };
 
-export default function Photos() {
-  const [images, setImages] = useState<Image[]>([]);
-
-  useEffect(() => {
-    const getImages = async () => {
-      const { data, error } = await supabase.storage
-        .from("my-bucket")
-        .list("my-folder");
-
-      if (error) {
-        console.error("Error fetching images:", error);
-      } else if (data) {
-        setImages(data);
-      }
-    };
-
-    getImages();
-  }, []);
+export default function Page({ images }: { images: Image[] }) {
   return (
     <Layout title="Fotografias" color="bg-[#202023]">
       <section className="mx-8 lg:mx-16 xl:mx-24 h-full ">
@@ -35,4 +17,22 @@ export default function Photos() {
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const { data, error } = await supabase.storage
+    .from("Fotografias")
+    .list("DSC");
+
+  if (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      images: data,
+    },
+  };
 }
